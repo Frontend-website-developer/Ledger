586afecb77be009f62bbba3739424fc9bd9ff5c8
Syed Sajid Ali <ssajidali.dev@gmail.com>
2026-08-07 17:12:21 +0500

    Origin update

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 8080;
 app.use(express.json());
 app.use(cors({
   origin: [
-    'http://localhost:5173',
+    'http://localhost:5174',
     'https://zealous-mud-0ec094800.7.azurestaticapps.net',
     'https://ledger.syedsajidali.dev'
   ],
